e7d83ded795d4779c1bb6bfe87ccf8d874a2f216
Yogesh Verma <yverma132@gmail.com>
2023-12-22 09:46:01 +0200

    Update gnn.py

--- models/gnn.py
+++ models/gnn.py
@@ -1,17 +1,18 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import PNAConv, global_add_pool, global_mean_pool, GCNConv
+from torch_geometric.nn import PNAConv, global_add_pool, global_mean_pool, GCNConv,GPSConv,TransformerConv
 from torchdiffeq import odeint as odeint
-#from layers.gin_layer import GINLayer
-#from layers.graph_convolution_layer import GCNLayer
-#from models.gnn_factory.gcn_layer_factory import GcnCreator
-#from models.gnn_factory.gin_layer_factory import GinCreator
-#from models.gnn_factory.pna_layer_factory import PnaCreator
+from layers.gin_layer import GINLayer
+from layers.graph_convolution_layer import GCNLayer
+from models.gnn_factory.gcn_layer_factory import GcnCreator
+from models.gnn_factory.gin_layer_factory import GinCreator
+from models.gnn_factory.pna_layer_factory import PnaCreator
 from torch_geometric.data import Data
 from torch_geometric.data import DataLoader
+from models.egnn_clean import EGNN
 
-'''
+
 class GNN(nn.Module):
     def __init__(
         self,
@@ -63,7 +64,66 @@ class GNN(nn.Module):
         x = self.classif(x)
         return x
 
-'''
+
+class GNN_cont(nn.Module):
+    def __init__(
+        self,
+        gnn,
+        hidden_dim,
+        depth,
+        num_node_features,
+        num_classes,
+        n_steps,
+        solver,
+        global_pooling,
+        deg=None,
+        batch_norm=True,
+    ):
+        super().__init__()
+        if gnn == "gin":
+            gnn_instance = GinCreator(hidden_dim, batch_norm)
+        elif gnn == "gcn":
+            gnn_instance = GcnCreator(hidden_dim, batch_norm)
+
+        build_gnn_layer = gnn_instance.return_gnn_instance
+
+        #self.pooling_fun = graph_pooling_operation
+        self.embedding = torch.nn.Linear(num_node_features, hidden_dim)
+
+        self.ode_embed = torch.nn.Linear(hidden_dim+1, hidden_dim)
+        #layers = []
+        #for i in range(depth):
+        layers = [TransformerConv(hidden_dim+1,hidden_dim,head=1)]
+        #layers = [build_gnn_layer(is_last=i == (depth - 1)) for i in range(depth)]
+
+        self.layers = nn.ModuleList(layers)
+        self.n_steps = n_steps
+        self.solver = solver
+
+        self.edge_index = 0
+
+    def ode(self,t,data):
+        tt = torch.ones_like(data[:, :1]) * t
+        x = torch.cat([tt.float(), data], 1)
+        for idx,layer in enumerate(self.layers):
+            #if idx==0:
+            #    x = layer(self.ode_embed(x),edge_index=self.edge_index)
+            #else: 
+            x = layer(x, edge_index=self.edge_index)
+
+        return x
+    
+
+    def forward(self, data):
+        x, self.edge_index = data.x, data.edge_index
+        time_steps = torch.linspace(0,1,steps=self.n_steps)
+        ode_rhs  = lambda t,x: self.ode(t,x)
+        x = self.embedding(x.float())
+        x = odeint(ode_rhs,x,time_steps,method=self.solver,atol=1e-4,rtol=1e-4)
+        #x = self.pooling_fun(x, data.batch)
+        #x = self.classif(x)
+        return x
+
 
 class EGNN_cont(nn.Module):
     def __init__(
@@ -176,14 +236,74 @@ class EGNN_cont(nn.Module):
 
     def forward(self, data):
         x, self.edge_index,pos,self.edge_attr = data.x, data.edge_index,data.pos,data.edge_attr
+        time_steps = torch.linspace(0,2,steps=self.n_steps)
+        ode_rhs  = lambda t,x: self.ode(t,x)
+        x = self.embedding(x.float())
+        x = torch.cat([x,pos],dim=1)
+        x = odeint(ode_rhs,x,time_steps,method=self.solver,atol=1e-3,rtol=1e-3)
+        node_feat,coords  = x[:,:,:-3],x[-1,:,-3:]
+        return node_feat
+    
+class SSP(torch.nn.Softplus):
+    def __init__(self, beta=1, threshold=20):
+        super(SSP, self).__init__(beta, threshold)
+    def forward(self, input):
+        sp0 = F.softplus(torch.zeros(1), self.beta, self.threshold).item()
+        return F.softplus(input, self.beta, self.threshold) - sp0
+
+
+class EGNN_cont_v2(nn.Module):
+    def __init__(
+        self,
+        #gnn,
+        hidden_dim,
+        #depth,
+        num_node_features,
+        #num_classes,
+        global_pooling,
+        n_steps,
+        solver,
+        edge_nf
+        #deg=None,
+        #batch_norm=True,
+    ):
+        super().__init__()
+        if global_pooling == "mean":
+            graph_pooling_operation = global_mean_pool
+        elif global_pooling == "sum":
+            graph_pooling_operation = global_add_pool
+
+        self.pooling_fun = graph_pooling_operation
+        self.embedding = torch.nn.Linear(num_node_features, hidden_dim)
+
+        self.n_steps = n_steps
+        self.solver = solver
+
+        self.egnn_model = EGNN(in_node_nf=hidden_dim,hidden_nf = hidden_dim,out_node_nf= hidden_dim,in_edge_nf=edge_nf,act_fn=SSP(),n_layers=1,residual=False)
+        self.edge_index = 0
+        self.coords_weight=1.0
+        self.edge_attr = 0
+
+    def ode(self,t,data):
+        x,pos = data[:,:-3],data[:,-3:]
+        tt = torch.ones_like(x[:, :1]) * t
+        dx_final = torch.cat([tt.float(), x], 1)
+        h,x = self.egnn_model(h=x,x=pos,edges=self.edge_index, edge_attr=self.edge_attr)
+        h = torch.cat([h,x],1)
+        return h
+    
+
+    def forward(self, data):
+        x, self.edge_index,pos,self.edge_attr = data.x, data.edge_index,data.pos,data.edge_attr
         time_steps = torch.linspace(0,1,steps=self.n_steps)
         ode_rhs  = lambda t,x: self.ode(t,x)
         x = self.embedding(x.float())
         x = torch.cat([x,pos],dim=1)
-        x = odeint(ode_rhs,x,time_steps,method=self.solver,atol=1e-4,rtol=1e-4)
+        x = odeint(ode_rhs,x,time_steps,method=self.solver,atol=1e-1,rtol=1e-1)
         node_feat,coords  = x[:,:,:-3],x[-1,:,-3:]
         return node_feat
-    
+
+
 
 # Used in the toy example
 class GCN(torch.nn.Module):
